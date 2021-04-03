e0ceafd71209be54810c1cf967e23b6fff140d47
Abel Orode <48480130+AbelOrode@users.noreply.github.com>
2021-04-04 00:59:41 +0100

    Authenticated endpoints with JWT working

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Api\SectionController;
 use App\Http\Controllers\Api\StudentController;
 use App\Http\Controllers\Api\SubjectController;
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 //Class API route
@@ -20,3 +21,18 @@ Route::ApiResource('/section', SectionController::class);
 
 //Student API route
 Route::ApiResource('/students', StudentController::class);
+
+
+
+//JWT Authentication route
+Route::group([
+    'prefix' => 'auth'
+], function (){
+
+//JWT Routes
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::get('/me', [AuthController::class, 'me']);
+});
